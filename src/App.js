9804b4b437ac517adc36747bbca3542dfa5e3e28
dahby <dstoll243@gmail.com>
2019-08-20 12:16:02 -0700

    proved back end is hittable from front end

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function App() {
+  const getSms = () => {
+    fetch('https://twilio-quote-generator-node.herokuapp.com/quote');
+  };
+
   return (
     <div className="App">
-      Everything goes here
+      <button onClick={getSms}>SEND TEXT!!!</button>
     </div>
   );
 }

d3599cfddeee25fe9c0ea1fc28386cb821cd8a44
dahby <dstoll243@gmail.com>
2019-08-31 11:15:03 -0700

    proof of life with signup form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import './App.css';
 
 function App() {
-  const getSms = () => {
-    fetch('https://twilio-quote-generator-node.herokuapp.com/quote');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signup = (event) => {
+    event.preventDefault();
+    console.log('signup fired');
+    // eslint-disable-next-line no-undef
+    fetch('http://localhost:3005/api/v1/users/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then(res => res.json())
+      .then(json => console.log(json));
   };
 
   return (
     <div className="App">
-      <button onClick={getSms}>SEND TEXT!!!</button>
+      <Form onSubmit={signup}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={(event) => setEmail(event.target.value)}/>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
+        </Form.Group>
+        <Form.Group controlId="formBasicChecbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
